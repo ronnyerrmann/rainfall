a73107b9ec9c523e1fc2ca8f84bd2b1d9b2edc91
ronnyerrmann <36272471+ronnyerrmann@users.noreply.github.com>
2025-08-03 14:22:43 +0100

    Create pdfs with daily images using pdflatex

--- rainfall_images.py
+++ rainfall_images.py
@@ -4,6 +4,7 @@ import re
 import requests
 import sys
 import numpy as np
+from pathlib import Path
 from PIL import Image
 from datetime import datetime, timedelta
 from collections import defaultdict
@@ -12,6 +13,21 @@ import cv2
 # Configuration
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 IMAGE_DIR = os.path.join(SCRIPT_DIR, "image_data")
+DAILY_SUB_DIR = os.path.join(SCRIPT_DIR, "subframes_daily")
+CONVERSIONS = [
+    # as 'P', avg, 33%ile mm, min mm, max mm, palette from palette = img.getpalette(); print([tuple(palette[i:i+3]) for i in range(0, len(palette), 3)]), legend text
+    [0, 0, 0, 0, 0.1, (0, 0, 0)],
+    [1, 0.3, 0.3, 0.1, 0.5, (0, 0, 254), "<0.5"],  # <0.5    blue
+    [4, 0.7, 0.7, 0.5, 1, (50, 101, 254), "0.5-1"],  # 0.5-1   lighter blue
+    [3, 1.5, 1.3, 1, 2, (12, 188, 254), "1-2"],  # 1-2     light blue
+    [2, 3, 2.6, 2, 4, (0, 163, 0), "2-4"],  # 2-4     green
+    [8, 6, 5.3, 4, 8, (254, 203, 0), "4-8"],  # 4-8     yellow
+    [7, 12, 10.6, 8, 16, (254, 152, 0), "8-16"],  # 8-16    orange
+    [6, 24, 21.3, 16, 32, (254, 0, 0), "16-32"],  # 16-32   red
+    [5, 48, 42.6, 32, 64, (179, 0, 0), "32-64"],  # >32     dark red
+    [9, 96, 85.3, 64, 128, (171, 32, 253), "64-128"],  # 64-128  purple
+    [10, 192, 170.6, 128, 999, (255, 255, 255), ">128"],  # >128    white
+]
 
 
 class DownloadFiles:
@@ -24,7 +40,7 @@ class DownloadFiles:
     def __init__(self):
         # Create download directory if it doesn't exist
         os.makedirs(IMAGE_DIR, exist_ok=True)
-    
+
     @staticmethod
     def floor_to_nearest_five(dt):
         floored_minute = dt.minute - (dt.minute % 5)
@@ -64,38 +80,23 @@ class DownloadFiles:
 
 class ProcessImages:
     DAILY_SUM_DIR = os.path.join(SCRIPT_DIR, "image_daily")
-    DAILY_SUB_DIR = os.path.join(SCRIPT_DIR, "subframes_daily")
     VIDEO_DIR = os.path.join(SCRIPT_DIR, "videos")
     # Regex to extract datetime from filename
     filename_re = re.compile(r'(\d{4}-\d{2}-\d{2}T\d{2}\:\d{2}\:00Z)\.png')
     filename_daily_re = re.compile(r'(\d{4}-\d{2}-\d{2})_sum(_cur)?\.png')
-    SUBAREAS = [] # x, y (irfan, start at 0), lon, lat, px_box_size/2, scale up, name # Todo: calculate px position from lat/lon
+    SUBAREAS = []  # x, y (irfan, start at 0), lon, lat, px_box_size/2, scale up, name # Todo: calculate px position from lat/lon
     SUBAREAS.append([2623, 2011, -0.119305, 51.509704, 30, 2, "London"])
     SUBAREAS.append([2363, 2019, -2.585907, 51.458285, 5, 2, "Bristol"])
     SUBAREAS.append([2649, 1915, 0.12291, 52.207607, 5, 2, "Cambridge"])
     SUBAREAS.append([2189, 1376, -4.31428, 55.93901, 1, 5, "Milngavie"])
     SUBAREAS.append([2098, 1240, -5.095253, 56.822303, 1, 5, "Fort William"])
-    SUBAREAS_FILENAME = os.path.join(SCRIPT_DIR, "subareas.csv")    # more entries as csv, not included in the python script for privacy reasons
+    SUBAREAS_FILENAME = os.path.join(SCRIPT_DIR,
+                                     "subareas.csv")  # more entries as csv, not included in the python script for privacy reasons
     SUBAREAS_TYPE = [int, int, float, float, int, int, str]
-    
-    conversions = [ # as 'P', avg, 33%ile mm, min mm, max mm, palette from palette = img.getpalette(); print([tuple(palette[i:i+3]) for i in range(0, len(palette), 3)])
-    [ 0,   0,   0,     0,   0.1, (0, 0, 0)],
-    [ 1,   0.3, 0.3,   0.1, 0.5, (0, 0, 254)],    # <0.5    blue
-    [ 4,   0.7, 0.7,   0.5, 1,   (50, 101, 254)], # 0.5-1   lighter blue
-    [ 3,   1.5, 1.3,   1,   2,   (12, 188, 254)], # 1-2     light blue
-    [ 2,   3,   2.6,   2,   4,   (0, 163, 0)],    # 2-4     green
-    [ 8,   6,   5.3,   4,   8,   (254, 203, 0)],  # 4-8     yellow
-    [ 7,  12,  10.6,   8,  16,   (254, 152, 0)],  # 8-16    orange
-    [ 6,  24,  21.3,  16,  32,   (254, 0, 0)],    # 16-32   red
-    [ 5,  48,  42.6,  32,  64,   (179, 0, 0)],    # >32     dark red
-    [ 9,  96,  85.3,  64, 128,   (171, 32, 253)], # 64-128  purple
-    [10, 192, 170.6, 128, 999,   (255, 255, 255)],# >128    white
-    ]
-
 
     def __init__(self):
         os.makedirs(self.DAILY_SUM_DIR, exist_ok=True)
-        os.makedirs(self.DAILY_SUB_DIR, exist_ok=True)
+        os.makedirs(DAILY_SUB_DIR, exist_ok=True)
         os.makedirs(self.VIDEO_DIR, exist_ok=True)
         if os.path.exists(self.SUBAREAS_FILENAME):
             print(f"Will use sub areas from {self.SUBAREAS_FILENAME}")
@@ -110,10 +111,11 @@ class ProcessImages:
                         self.SUBAREAS.append(parsed_row)
                     except ValueError as e:
                         print(f"Skipping row due to conversion error: {row} ({e})")
-        self.palette = [0] * len(self.conversions) * 3
-        for conversion in self.conversions:
+        self.palette = [0] * len(CONVERSIONS) * 3
+        for conversion in CONVERSIONS:
             for i in range(3):
-                self.palette[conversion[0]*3+i] = conversion[5][i]   # palette needs to be in increasing number order, but for my convenience conversions are in the order of increasing values
+                self.palette[conversion[0] * 3 + i] = conversion[5][
+                    i]  # palette needs to be in increasing number order, but for my convenience CONVERSIONS are in the order of increasing values
 
     @staticmethod
     def parse_datetime(s):
@@ -134,7 +136,7 @@ class ProcessImages:
         for frame in rgb_frames:
             out.write(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))  # OpenCV uses BGR
         out.release()
-    
+
     def run(self):
         # Organize images by day
         images_by_day = defaultdict(list)
@@ -148,12 +150,12 @@ class ProcessImages:
             if f"{day}_sum.png" not in images.values():
                 images_by_day[day].append((dt, os.path.join(IMAGE_DIR, fname)))
         self.process_days(images_by_day)
-        
+
         # Create summary video from summed images
         images = self.list_day_sum_images()
         if len(images.keys()):
             self.process_summaries(images)
-        
+
         print(f"{datetime.now().time()} Done.")
 
     def list_day_sum_images(self):
@@ -178,32 +180,32 @@ class ProcessImages:
                 img = Image.open(path)  # keep as "P" as it's a colour palette. Convert to decimals using .convert("L")
                 arr = np.array(img, dtype=np.uint8)
                 deci_arr = np.zeros_like(arr, dtype=np.float64)
-                for conversion in self.conversions:
-                    deci_arr[arr==conversion[0]] = conversion[2]
+                for conversion in CONVERSIONS:
+                    deci_arr[arr == conversion[0]] = conversion[2]
                 deci_arr /= 12  # 5min are a 1/12th of an hour to convert mm/hour into mm
-                #print(arr.shape, np.unique(arr,return_counts=True))
+                # print(arr.shape, np.unique(arr,return_counts=True))
 
                 # Sum up images
                 if accum is None:
                     accum = np.zeros_like(deci_arr, dtype=np.float64)
                 accum += deci_arr
-                
+
                 # Extract subsections
                 for (x, y, _, _, size, _, name) in self.SUBAREAS:
                     if name not in frames.keys():
-                        frames[name] = np.zeros((len(day_files), 2*size+1, 2*size+1), dtype=np.uint8)
-                    frames[name][i,:,:] = arr[y-size:y+size+1, x-size:x+size+1]
-                    #if name == "Home":
-                       # print(name, frames[name][i,:,:]) # check what needs to be done to the values
-            
+                        frames[name] = np.zeros((len(day_files), 2 * size + 1, 2 * size + 1), dtype=np.uint8)
+                    frames[name][i, :, :] = arr[y - size:y + size + 1, x - size:x + size + 1]
+                    # if name == "Home":
+                    # print(name, frames[name][i,:,:]) # check what needs to be done to the values
+
             # Save daily summed image as grey-scale mm and with colour palette
             summed_img = np.clip(accum, 0, 255).astype(np.uint8)
             Image.fromarray(summed_img).save(f"{self.DAILY_SUM_DIR}/greyscale-mm_{day}_sum.png")
             # Save daily summed image as with colour palette
             # accum *= 0.1    # Show the colour scale in cm instead of mm
             summed_img = np.zeros_like(accum, dtype=np.uint8)
-            for conversion in self.conversions:
-                summed_img[(accum>=conversion[3]) & (accum<conversion[4])] = conversion[0]
+            for conversion in CONVERSIONS:
+                summed_img[(accum >= conversion[3]) & (accum < conversion[4])] = conversion[0]
             img = Image.fromarray(summed_img).convert('P')
             img.putpalette(self.palette)
             # Mark if it's the current day
@@ -211,7 +213,7 @@ class ProcessImages:
             if os.path.exists(fname_current_day):
                 os.remove(fname_current_day)
             img.save(fname_current_day if day == datetime.utcnow().date() else f"{self.DAILY_SUM_DIR}/{day}_sum.png")
-            
+
             # Write daily videos
             for name, subframes in frames.items():
                 video_path = f"{self.VIDEO_DIR}/{name.replace(' ', '_')}_{day}.mp4"
@@ -222,21 +224,116 @@ class ProcessImages:
         frames = {}
         for i, (day, fname) in enumerate(sorted(images.items())):
             arr = np.array(Image.open(os.path.join(self.DAILY_SUM_DIR, fname)), dtype=np.uint8)
-                
+
             for (x, y, _, _, size, scale, name) in self.SUBAREAS:
-                arr_sub = arr[y-size:y+size+1, x-size:x+size+1]
+                arr_sub = arr[y - size:y + size + 1, x - size:x + size + 1]
                 if name not in frames.keys():
-                    frames[name] = np.zeros((len(images), 2*size+1, 2*size+1), dtype=np.uint8)
-                frames[name][i,:,:] = arr_sub
+                    frames[name] = np.zeros((len(images), 2 * size + 1, 2 * size + 1), dtype=np.uint8)
+                frames[name][i, :, :] = arr_sub
                 # Save the daily subframes with colour palette
                 img = Image.fromarray(arr_sub).convert('P')
                 img = img.resize((arr_sub.shape[1] * scale, arr_sub.shape[0] * scale), resample=Image.NEAREST)
                 img.putpalette(self.palette)
-                img.save(f"{self.DAILY_SUB_DIR}/{name.replace(' ', '_')}_{day}.png")
+                img.save(f"{DAILY_SUB_DIR}/{name.replace(' ', '_')}_{day}.png")
 
         for name, subframes in frames.items():
             video_path = f"{self.VIDEO_DIR}/{name.replace(' ', '_')}.mp4"
             self.save_video_palette(subframes, video_path)
 
-DownloadFiles().run()
-ProcessImages().run()
+
+class MakePdf:
+    texFileNameBase = "rainfall.tex"
+    framesPerRow = 7
+    rowsPerPage = 10
+    filename_daily_re = re.compile(r'(.*)_(\d{4}-\d{2}-\d{2})\.png')
+    TEX_DIR = os.path.join(SCRIPT_DIR, "tex")
+
+    def __init__(self):
+        os.makedirs(self.TEX_DIR, exist_ok=True)
+        os.chdir(self.TEX_DIR)
+        self.legend_items = [(conv[5], conv[6].replace("<", "$<$").replace(">", "$>$")) for conv in CONVERSIONS if conv[0] > 0]
+
+    def list_day_sub_images(self):
+        images = {}
+        for fname in sorted(os.listdir(DAILY_SUB_DIR)):
+            match = self.filename_daily_re.match(fname)
+            if not match:
+                continue
+            name = match.group(1)
+            day = match.group(2)
+            if name not in images.keys():
+                images[name] = {}
+            images[name][day] = os.path.join(DAILY_SUB_DIR, fname)
+        return images
+
+    def write_end_and_legend(self, f):
+        f.write(r"\end{tabular}\end{figure}" + "\n")
+        f.write(r"\noindent\begin{tikzpicture}[x=1cm, y=1cm]" + "\n")
+        for i, (color, label) in enumerate(self.legend_items):
+            if i == len(self.legend_items) - 1:
+                label += " mm/d"
+            x = i * 1.7
+            f.write(f"""  \\definecolor{{c{i}}}{{RGB}}{{{color[0]}, {color[1]}, {color[2]}}}
+  \\filldraw[fill=c{i}, draw=black] ({x}, 0) rectangle ({x + 0.5}, 0.5);
+  \\node[right] at ({x + 0.6}, 0.25) {{\\small {label}}};
+""")
+        f.write(r"\end{tikzpicture}" + "\n")
+
+    def create_latex(self):
+        images = self.list_day_sub_images()
+        for name, img in images.items():
+            texName = f"{name}_{self.texFileNameBase}"
+            with open(texName, "w") as f:
+                print(os.path.basename(__file__), name, datetime.now().strftime("%y%m%d%H%M%S"), "")
+                f.write(r"""\pdfinfo{
+   /Author (%s)
+   /Title  (Daily rainfall for %s)
+   /CreationDate (D:%s)
+}
+\documentclass[a4paper]{article}
+\usepackage[margin=0.5in]{geometry}
+\usepackage{graphicx}
+\usepackage{tikz}
+\pagestyle{empty}
+
+\newcommand{\subf}[2]{%%
+  {\small\begin{tabular}[t]{@{}c@{}}
+  #1\\#2
+  \end{tabular}}%%
+}
+
+\begin{document}
+""" % (os.path.basename(__file__), name, datetime.now().strftime("%y%m%d%H%M%S")) )
+                rows = 0
+                for i, (day, fname) in enumerate(img.items()):
+                    posOnRow = i % self.framesPerRow
+                    if posOnRow == 0 and i != 0:
+                        # f.write(r"\vspace{0.2cm}\newline" + "\n")  # new row
+                        rows += 1
+                        if rows == self.rowsPerPage:
+                            # new page needed
+                            self.write_end_and_legend(f)
+                            f.write(r"\newpage" + "\n")
+                            rows = 0
+                    if posOnRow == 0 and rows == 0:  # this is a new page
+                        # f.write(r"" + name + "\n")    # write place name
+                        f.write(r"""\noindent\begin{figure}[t!]\centering
+\begin{tabular}{%s}
+""" % ("c" * self.framesPerRow))
+                    f.write(r"  \subf{\includegraphics[width=0.12\linewidth]{\detokenize{" + fname + "}}}{" + day + "}" +
+                            ("&" if posOnRow < self.framesPerRow - 1 else "\\\\") + "\n"  # middle or last entry
+                            )
+                self.write_end_and_legend(f)
+
+                f.write(r"\end{document}")
+            self.compile_latex(texName)
+
+    def compile_latex(self, texName):
+        os.system(f"pdflatex -interaction=batchmode {texName}")
+
+
+if __name__ == "__main__":
+    DownloadFiles().run()
+    ProcessImages().run()
+    toPdf = MakePdf()
+    toPdf.create_latex()
